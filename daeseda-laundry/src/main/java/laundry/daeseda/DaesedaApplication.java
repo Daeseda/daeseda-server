caf4d56b3d866eb1406348a142ba3e4e805a33a0
minwook <zkvn1103@naver.com>
2023-09-13 11:55:04 +0900

    Initial commit
    Build - gradle, Spring Boot - 2.7.15
    Java - 11, Packaging - Jar

---
+++ daeseda-laundry/src/main/java/laundry/daeseda/DaesedaApplication.java
@@ -0,0 +1,13 @@
+package laundry.daeseda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaesedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaesedaApplication.class, args);
+	}
+
+}
---
+++ daeseda-laundry/src/test/java/laundry/daeseda/DaesedaApplicationTests.java
@@ -0,0 +1,13 @@
+package laundry.daeseda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaesedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
